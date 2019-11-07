8a6f2bc85572009800a4a97213853bfe2d23d4f8
bhavesh narola <bhavesh@abacus.co>
2019-11-07 22:11:33 +1100

    paggination done
    Paggination done

--- SafetracWebApp/Controllers/UsersController.cs
+++ SafetracWebApp/Controllers/UsersController.cs
@@ -21,5 +21,27 @@ namespace SafetracWebApp.Controllers
         {
             return Json(new { data = userModel.GetAllUsers() }, JsonRequestBehavior.AllowGet);
         }
+        public ActionResult UsersList()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult LoadAllUsersData()
+
+        {
+            var draw = Request.Form.GetValues("draw").FirstOrDefault();
+            var start = Request.Form.GetValues("start").FirstOrDefault();
+            var length = Request.Form.GetValues("length").FirstOrDefault();
+            var search = Request.Form.GetValues("search[value]").FirstOrDefault();
+            //Find Order Column
+            var sortColumn = Request.Form.GetValues("columns[" + Request.Form.GetValues("order[0][column]").FirstOrDefault() + "][name]").FirstOrDefault();
+            var sortColumnDir = Request.Form.GetValues("order[0][dir]").FirstOrDefault();
+            int pageSize = length != null ? Convert.ToInt32(length) : 0;
+            int PageNumber = start != null ? Convert.ToInt32(start) / Convert.ToInt32(length) + 1 : 0;
+
+            var lstUsers = userModel.LoadAllUsers(search.ToString(), PageNumber, pageSize, sortColumn, sortColumnDir);
+            int TotalRecord = lstUsers.Select(x => x.MaxRows.Value).First();
+            return Json(new { draw = draw, recordsFiltered = TotalRecord, recordsTotal = TotalRecord, data = lstUsers }, JsonRequestBehavior.AllowGet);
+        }
     }
 }
--- SafetracWebApp/Models/UserModelView.cs
+++ SafetracWebApp/Models/UserModelView.cs
@@ -48,5 +48,6 @@ namespace SafetracWebApp.Models
             }
 
         }
+
     }
 }
